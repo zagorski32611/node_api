eff9c23098a42a4515d21a1ba9241e9e2da06f96
zagorski32611 <j.j.zagorski@gmail.com>
2017-12-08 18:37:02 -0500

    added .get function

--- server.js
+++ server.js
@@ -68,7 +68,33 @@ router.route('/bears/:bear_id')
       });
     });
 
+    // update bear by id
 
+    .put(function(req, res) {
+
+      Bear.findById(req.params.bear_id, function(err, bear) {
+        if (err)
+          res.send(err);
+        bear.name = req.body.name;
+
+        bear.save(function(err) {
+          if (err)
+            res.send(err);
+          res.json({message: "bear updated!"});
+        });
+
+      });
+    });
+
+router.route('/bears/:bear_id')
+
+    .get(function(req, res) {
+      Bear.findById(req.params.bear_id, function(err, bear) {
+        if (err)
+          res.send(err);
+        res.json(bear);
+      });
+    });
 
 // Register our Routes
 
